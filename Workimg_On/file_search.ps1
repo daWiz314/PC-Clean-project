ef6418ed044384b51a0b576b595194c5e7039e06
Harry <50802261+daWiz314@users.noreply.github.com>
2024-04-05 10:52:31 -0400

    Pushing to transfer to different unit

--- Workimg_On/file_search.ps1
+++ Workimg_On/file_search.ps1
@@ -14,30 +14,42 @@
 #       Every time this opens, it will check the cache file and update it
 #
 # 
-# ----------------------------------------------------------------------------------
-#               Classes
-# ----------------------------------------------------------------------------------
-# Class File
-# Just a container for a file object
+# ------------------------------------------------------------------------------
 #
+#               Classes
+# ------------------------------------------------------------------------------
+# Class File
+#   Just a container for a file object
+#   METHODS:
+#       get_path() - Returns the path of the file
+
 # Class Folder
-# Contains a directory and all of its children
-# It is linked to the parent object, by the parent being passed into object.
-# This way we can crawl any direction from a folder
-# Will we actually do this? No. But it's cool.
+#   Contains a directory and all of its children
+#   It is linked to the parent object, by the parent being passed into object.
+#   This way we can crawl any direction from a folder
+#   Will we actually do this? No. But it's cool.
+#   METHODS:
+#      get_path() - Returns the path of the folder
 #
 class File {
-    [folder]$parent
+    [Folder]$parent
     [string]$name
+    [string]$drive
 
-    file([folder]$folder, [string]$name) {
+    File([Folder]$folder, [string]$drive, [string]$name) {
         $this.parent = $folder
         $this.name = $name
+        $this.drive = $drive
+    }
+
+    [string]get_path() {
+        return ($this.parent.path + "\" + $this.name)
     }
 }
 
 
 class Folder {
+    [string]$drive
     [string]$path
     [string]$name
     [folder]$parent
@@ -45,23 +57,55 @@ class Folder {
     [File[]]$files
     [string[]]$contents
 
-    Folder([folder]$parent=$null, [string]$name) {
+    Folder([Folder]$parent, [string]$drive, [string]$name) {
         # If the parent directory is null, then we are at the root
         if($parent -eq $null) {
-            # Grab the path to figure out which drive we are in
-            # Then split it up, to just the letter and extra stuff we don't need
-            $this.path = ((pwd)-split ":") -replace "\\", ""
-            $this.parent = $this.path.Trim() + ":\"
-        }
-        $this.path = $parent.path + "\" + $name
+            $this.name = ""
+            $this.path = $drive
+        } else {
+            # If we are not at the root,
+            # Then we need to get the path from the parent
+            # If the parent path ends in a \, then we don't need to add one
+            if ($parent.path -match "\\$") {
+                $this.path = $parent.path + $name
+            } else {
+                $this.path = $parent.path + "\" + $name
+            }
+            $this.drive = $drive
+        }        
         $this.name = $name
         $this.parent = $parent
         $this.contents = dir $this.path
     }
+
+    [string]get_path() {
+        if ($this.parent -eq $null) {
+            return $this.path
+        } else {
+            return ($this.parent.path + "\" + $this.name)
+        }
+    }
+
+    discover_children() {
+        foreach ($content in $this.contents) {
+            if ($content -is [System.IO.DirectoryInfo]) {
+                $this.children_folders.Add([Folder]::new($this, $this.drive, $content.Name))
+            } else {
+                #$this.files.Add([File]::new($this, $this.drive, $content.Name))
+            }
+        }
+        # Try to do some clean up
+        $this.contents = $null
+    }
+
+    [Folder[]]get_children() {
+        return $this.children_folders
+    }
 }
-# ----------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+#
 #            Functions
-# ----------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Function Main
 # Main function that is body of script
 # After finding all drives, starts a Master Job controller, 
@@ -77,36 +121,134 @@ function main {
     $container = ($container | Where-Object {$_ -match "\s\w{1,2}"}) -replace "\\", ""
     $container = $container.Trim()
 
-    cd \
-    $test = [folder]::new($null, "Users")
-    Write-Host $test
 
-}
+    foreach($drive in $container) {
+        $folder_container.Add([Folder]::new($null, $drive+":\", $null), $false)
+    }
 
-# ----------------------------------------------------------------------------------
+    # RunspaceFactoryPool that runs through for uncrawled items and crawls them
+    $list_adder = [RunspaceFactory]::CreateRunspacePool(1, 5)
+    $list_adder.Open()
+
+    $list_adder_script = {
+        param($num)
+        $cacheData[$num] = $false
+    }
+
+    # Actually start job
+    foreach ($num in 1..255) {
+        $test = [Powershell]::Create().AddScript($list_adder_script).AddArgument($num)
+        $test.RunspacePool = $list_adder
+        $test.BeginInvoke()
+    }
+    $list_adder.Close()
+    $list_adder.Dispose() # Should be done here
+}   
+
+# ------------------------------------------------------------------------------
+#
 #            Variables
-# ----------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Variable: cacheData
 #   Contains all the data from the cache if there is one available
 #   This is the main data store for the program
 #   Data outline
-#                       [KEY] : [VALUE]
-#     [file/folder indicator] : [object pertaining to file or folder]
+#                     [KEY] : [VALUE]
+#   [file/folder indicator] : [object pertaining to file or folder]
 
-# Variable: syncData
-#   Contains all the new data from the threads
-#   This is to update the cache as needed
-#   ----------Same data outline as above----------
+# Variable: folder_container
+#   Master Container for all the folders, and if they are sorted or not
+#   Data outline
+#                     [KEY] : [VALUE]
+#                  [Folder] : [bool on if it is sorted or not]
+
 #
+# Variable: sorted_list_alpha
+#   Contains all the data from the cache, but sorted alphabetically
+#   This is for easy access to the data when searching
+#                      [KEY] : [VALUE]
+#         [file/folder name] : [path to corresponding file/folder]
+#
+
+# Variable: sorted_list_tree
+#   Contains all the data from the cache, but sorted by tree
+#   This is for easy access to the data when searching
+#                      [KEY] : [VALUE]
+#         [file/folder name] : [path to corresponding file/folder]
+
 $cacheData = [Hashtable]::Synchronized(@{})
-$syncData = [Hashtable]::Synchronized(@{})
+$folder_container = [Hashtable]::Synchronized(@{})
+$sorted_list_alpha = [Hashtable]::Synchronized(@{})
+$sorted_list_tree = [Hashtable]::Synchronized(@{})
 
 
 
 
 
-
-# ----------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+#
 #         Actually run script
-# ----------------------------------------------------------------------------------
-main
+# ------------------------------------------------------------------------------
+#Measure-Command {main}
+
+#break point here
+Write-Host "Done!"
+Function Get-RunspaceData {
+    [cmdletbinding()]
+    param(
+        [switch]$Wait
+    )
+    Do {
+        $more = $false         
+        Foreach($runspace in $runspaces) {
+            If ($runspace.Runspace.isCompleted) {
+                $runspace.powershell.EndInvoke($runspace.Runspace)
+                $runspace.powershell.dispose()
+                $runspace.Runspace = $null
+                $runspace.powershell = $null                 
+            } ElseIf ($runspace.Runspace -ne $null) {
+                $more = $true
+            }
+        }
+        If ($more -AND $PSBoundParameters['Wait']) {
+            Start-Sleep -Milliseconds 100
+        }   
+        #Clean out unused runspace jobs
+        $temphash = $runspaces.clone()
+        $temphash | Where {
+            $_.runspace -eq $Null
+        } | ForEach {
+            Write-Verbose ("Removing {0}" -f $_.computer)
+            $Runspaces.remove($_)
+        }  
+        [console]::Title = ("Remaining Runspace Jobs: {0}" -f ((@($runspaces | Where {$_.Runspace -ne $Null}).Count)))             
+    } while ($more -AND $PSBoundParameters['Wait'])
+}
+$ScriptBlock = {
+    Param ($computer,$hash)
+    $hash[$Computer]=([int]$computer*10)
+}
+$Script:runspaces = New-Object System.Collections.ArrayList   
+$Computername = 1,2,3,4,5
+$hash = [hashtable]::Synchronized(@{})
+$sessionstate = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+$runspacepool = [runspacefactory]::CreateRunspacePool(1, 10, $sessionstate, $Host)
+$runspacepool.Open() 
+
+ForEach ($Computer in $Computername) {
+    #Create the powershell instance and supply the scriptblock with the other parameters 
+    $powershell = [powershell]::Create().AddScript($scriptBlock).AddArgument($computer).AddArgument($hash)
+           
+    #Add the runspace into the powershell instance
+    $powershell.RunspacePool = $runspacepool
+           
+    #Create a temporary collection for each runspace
+    $temp = "" | Select-Object PowerShell,Runspace,Computer
+    $Temp.Computer = $Computer
+    $temp.PowerShell = $powershell
+           
+    #Save the handle output when calling BeginInvoke() that will be used later to end the runspace
+    $temp.Runspace = $powershell.BeginInvoke()
+    Write-Verbose ("Adding {0} collection" -f $temp.Computer)
+    $runspaces.Add($temp) | Out-Null               
+}
