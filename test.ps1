ec006bfad071102323e0803538101d1f0c37f828
Harry <50802261+daWiz314@users.noreply.github.com>
2024-06-06 12:02:11 -0400

    Trying some trickery, couldn't get it will return later

--- test.ps1
+++ test.ps1
@@ -18,10 +18,53 @@ function add_spaces {
         [Parameter(Mandatory=$true)][int]$spaces
     )
     $test = "";
-    for($i=0; $i -lt $spaces; $i++) {
-        $test += " "
+    # for($i=0; $i -lt $spaces; $i++) {
+    #     $test += " "
+    # }
+    return $test += " " * $spaces
+
+}
+
+function compile_message {
+    param (
+        [Parameter(Mandatory=$true)][string[]]$messages,
+        [Parameter(Mandatory=$false)][int]$top=1,
+        [Parameter(Mandatory=$false)][int]$selection=$top+1
+    )
+    Clear-Host
+    $new_line = $Host.UI.RawUI.WindowSize.Width
+    $height = ($Host.UI.RawUI.WindowSize.Height/2) - $messages.Length
+    $center = $Height * $new_line
+    $final_message = " " * $center
+    foreach ($message in $messages) {
+        if($selection -eq $messages.IndexOf($message)) {
+            $space_around_word = ($Host.UI.RawUI.WindowSize.Width/2) - $message.Length + 1
+            $final_message += (" " * $space_around_word) + ">" + $message + (" " * $space_around_word)
+        } else {
+            $space_around_word = ($Host.UI.RawUI.WindowSize.Width/2) - $message.Length
+            $final_message += (" " * $space_around_word) + $message + (" " * $space_around_word)
+        }
+        
     }
-    return $test
+    Write-Host $final_message -NoNewline
+
+    $key = detectKeyPress
+    if ($key -eq "up") {
+        if ($selection -gt 0) {
+            $selection--
+        } else {
+            $selection = $messages.Length - 1
+        }
+    } elseif ($key -eq "down") {
+        if ($selection -lt ($messages.Length - 1)) {
+            $selection++
+        } else {
+            $selection = 0
+        }
+    } elseif ($key -eq "enter") {
+        return $selection
+    }
+    compile_message -messages $messages -selection $selection -top $top
 
 }
 
@@ -100,5 +143,5 @@ function detectKeyPress {
 
 }
 
-$messages = @("Hello", "World")
-display_message -messages $messages
+$messages = @("Hello", "World", "test", "test2")
+compile_message -messages $messages
